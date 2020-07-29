638d37266e793a052686d1e25e030100515d51d2
t0210smn <t0210smn@prestadorcbmp.com.br>
2020-07-29 09:55:14 -0300

    Alterações iniciais

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <p>
+        <a href="https://www.alura.com.br/">
+          Ongame
+        </a>
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
